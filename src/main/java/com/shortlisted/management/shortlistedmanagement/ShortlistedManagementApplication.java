6ed73ebd28dc42cab898e378e3ee97e86ceae509
Sachin <sachinkmr375@gmail.com>
2021-06-19 21:53:30 +0530

    init repo

---
+++ src/main/java/com/shortlisted/management/shortlistedmanagement/ShortlistedManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shortlisted.management.shortlistedmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlistedManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortlistedManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shortlisted/management/shortlistedmanagement/ShortlistedManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortlisted.management.shortlistedmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlistedManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
