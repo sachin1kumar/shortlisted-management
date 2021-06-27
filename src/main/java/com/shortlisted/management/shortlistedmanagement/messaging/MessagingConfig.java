921cddefebd6293056588ba0e5f7671f1296b7a5
Sachin <sachinkmr375@gmail.com>
2021-06-27 15:35:35 +0530

    rabbit mq setup successful

--- src/main/java/com/shortlisted/management/shortlistedmanagement/messaging/MessagingConfig.java
+++ src/main/java/com/shortlisted/management/shortlistedmanagement/messaging/MessagingConfig.java
@@ -1,44 +1,72 @@
 package com.shortlisted.management.shortlistedmanagement.messaging;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessagingConfig {
 
-    public static final String QUEUE = "shortlisted_books_queue";
-    public static final String EXCHANGE = "shortlisted_books_exchange";
-    public static final String ROUTING_KEY = "shortlisted_books_routingKey";
+    @Value("${spring.rabbitmq.queue}")
+    private String queue;
+
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.routingkey}")
+    private String routingKey;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
 
     @Bean
     public Queue queue() {
-        return new Queue(QUEUE);
+        return new Queue(queue, true);
     }
 
     @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
+    Exchange myExchange() {
+        return ExchangeBuilder.directExchange(exchange).durable(true).build();
     }
 
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(myExchange())
+                .with(routingKey)
+                .noargs();
     }
 
     @Bean
-    public MessageConverter converter() {
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
-    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(converter());
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
     }
 }
--- src/main/java/com/shortlisted/management/shortlistedmanagement/publishers/ShortlistPublisher.java
+++ src/main/java/com/shortlisted/management/shortlistedmanagement/publishers/ShortlistPublisher.java
@@ -5,23 +5,30 @@ import com.shortlisted.management.shortlistedmanagement.entities.ShortlistDetail
 import com.shortlisted.management.shortlistedmanagement.entities.ShortlistedStatus;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController()
+@RestController
 @RequestMapping("/shortlist")
 public class ShortlistPublisher {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.routingkey}")
+    private String routingKey;
+
     @PostMapping("/book")
     public ShortlistedStatus addBookToShorList(@RequestBody ShortlistDetails shortlistDetails) {
         ShortlistedStatus shortlistedStatus = new ShortlistedStatus("Shortlisted Successfully!");
-        rabbitTemplate.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY, shortlistDetails);
+        rabbitTemplate.convertAndSend(exchange, routingKey, shortlistDetails);
         return shortlistedStatus;
     }
 }
