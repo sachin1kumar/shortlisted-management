2452fa3d3d3359cb6291bbcca4f44cb7204df5b6
Sachin <sachinkmr375@gmail.com>
2021-07-03 23:03:08 +0530

    created log file in custom path.

--- src/main/java/com/shortlisted/management/shortlistedmanagement/publishers/ShortlistPublisher.java
+++ src/main/java/com/shortlisted/management/shortlistedmanagement/publishers/ShortlistPublisher.java
@@ -1,9 +1,13 @@
 package com.shortlisted.management.shortlistedmanagement.publishers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shortlisted.management.shortlistedmanagement.entities.Books;
 import com.shortlisted.management.shortlistedmanagement.entities.ShortlistDetails;
 import com.shortlisted.management.shortlistedmanagement.entities.ShortlistedStatus;
 import com.shortlisted.management.shortlistedmanagement.proxy.BookManagementProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,8 +33,12 @@ public class ShortlistPublisher {
     @Autowired
     private BookManagementProxy bookManagementProxy;
 
+    private final Logger logger = LoggerFactory.getLogger(ShortlistPublisher.class);
+
     @PostMapping("/book")
-    public ShortlistedStatus addBookToShorList(@RequestBody final ShortlistDetails shortlistDetails) {
+    public ShortlistedStatus addBookToShorList(@RequestBody final ShortlistDetails shortlistDetails)
+            throws JsonProcessingException {
+        logger.info("Shortlisted request : {}",new ObjectMapper().writeValueAsString(shortlistDetails));
         ShortlistedStatus shortlistedStatus = new ShortlistedStatus("Shortlisted Successfully!");
         rabbitTemplate.convertAndSend(exchange, addShortListedRoutingKey, shortlistDetails);
         return shortlistedStatus;
@@ -44,7 +52,10 @@ public class ShortlistPublisher {
     }
 
     @GetMapping("/getShortlistedBooks/{userId}")
-    public List<Books> getShortlistedBooks(@PathVariable final Long userId) {
-        return bookManagementProxy.retrieveExchangeValue(userId);
+    public List<Books> getShortlistedBooks(@PathVariable final Long userId) throws JsonProcessingException {
+        List<Books> booksList = bookManagementProxy.retrieveExchangeValue(userId);
+        logger.info("books-management response from shortlisted service: {}"
+                , new ObjectMapper().writeValueAsString(booksList));
+        return booksList;
     }
 }
